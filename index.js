4d70b1b8b5d358ed9902a25e881caa001cc628b0
ismaponto <isma.pontoriero@gmail.com>
2023-10-31 18:16:31 +0100

    j

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
