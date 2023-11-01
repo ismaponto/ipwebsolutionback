9f8828f4f01948597e06179137b942e3c2035d54
ismaponto <isma.pontoriero@gmail.com>
2023-11-01 16:22:32 +0100

    cambios

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -6,8 +6,7 @@ const nodemailer = require('nodemailer');
 
 function validarFormatoEmail(email) {
     // Expresión regular para validar el formato de correo electrónico
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     // Utiliza el método test para verificar si el email coincide con la expresión regular
     return regex.test(email);
 }
